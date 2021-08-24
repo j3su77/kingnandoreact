2585e1303033f49205a4b7a129e3d538019e5c66
J3su <kingnandoradio@gmail.com>
2021-08-23 23:06:25 -0500

    agregando banner publicidad

---
+++ src/components/AdsBanner.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Styles from "../components/styles-components/adsbanner.module.css"
+import advertisingImage from "../images/advertising.png"
+
+const AdsBanner = () => {
+    return (
+        <div className={Styles.container_ads}>
+        
+            <img className={Styles.imgAds} width="100%" src={advertisingImage} alt="" />
+      
+     
+    </div>
+    )
+}
+
+export default AdsBanner
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -4,6 +4,7 @@ import Chat from "./Chat";
 import Post from "./Posts";
 import Gallery from "./Gallery"
 import Styles from "./home.module.css";
+import AdsBanner from "../../AdsBanner";
 
 const Home = () => {
   useEffect(() => {
@@ -12,7 +13,9 @@ const Home = () => {
   return (
     <div className={Styles.container__home}>
       <Slider />
-  
+      <div className={Styles.AdsBanner}>
+      <AdsBanner   />
+      </div>
       <Chat />
       <Post />
       <Gallery />
